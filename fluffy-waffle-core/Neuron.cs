b200ed5bb3cefa225c14794597b26b1e7361b799
zhsir123 <zhsir123@gmail.com>
2017-10-19 20:50:19 +0900

    propagation 및 fairing 구현
    in_branch로 propagtion을 전달하고 out_branch로 propagation을 받아 시작한 neuron 으로 부터 처음 까지 올라감, 두 뉴런이 서로 in과 out을 지정 할경우 에러 발생, 왼쪽으로 페어링, 오른쪽으로 propagation

--- fluffy-waffle-core/Neuron.cs
+++ fluffy-waffle-core/Neuron.cs
@@ -10,18 +10,53 @@ namespace fluffy_waffle_core
 {
     public class Neuron : IDrawable
     {
-        public List<(Neuron to, float weight)> To;
+        public List<Neuron> InputBranch;
+        public List<(Neuron neuron, double weight)> OutputBranch;
 
         private Ellipse _shape;
         public UIElement Control => _shape;
         public Vector Position { get; set; }
+        public Double Value { get; set; }
 
         public Neuron(Vector pos)
         {
-            To = new List<(Neuron to, float weight)>();
+            InputBranch = new List<Neuron>();
+            OutputBranch = new List<(Neuron neuron, double weight)>();
+            // 초기 세팅, Value 는 test용
             Position = pos;
+            Value = 1;
             _shape = new Ellipse();
-            _shape.SetCircle(Position, 3);
+            _shape.SetCircle(Position, 30);
+            _shape.Stroke = System.Windows.Media.Brushes.Blue;
+            _shape.Fill = System.Windows.Media.Brushes.White;
+            _shape.Tag = this;
+        }
+
+        // 다른뉴런에서 클릭 됐을 때 추가
+        public void AppendInputBranch(Neuron neuron)
+        {
+            this.InputBranch.Add(neuron);
+
+        }
+        // 뉴런에서 다른뉴런으로 클릭 했을 때 추가
+        public void AppendOutputBranch(Neuron neuron)
+        {
+            double weight = new Random().NextDouble();
+            this.OutputBranch.Add((neuron, weight));          
+        }
+
+        // 각 Branch에 값을 전달
+        public void Propagation()
+        {
+            foreach (Neuron neuron in this.InputBranch)
+            {
+                neuron.Propagation();
+            }
+
+            foreach ((Neuron neuron, double weight) in this.OutputBranch)
+            {
+                neuron.Value += this.Value * weight;
+            }
         }
     }
 }
--- fluffy-waffle/MainWindow.xaml.cs
+++ fluffy-waffle/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,17 +20,59 @@ namespace fluffy_waffle
     public partial class MainWindow : Window
     {
         private List<IDrawable> _drawables = new List<IDrawable>();
+        List<Neuron> Neurons;
+        private List<Neuron> clicked;
 
         public MainWindow()
-        {
+        {   
             InitializeComponent();
-            AddDrawable(new Neuron(new Vector(100, 100)));
+
+            clicked = new List<Neuron>();
+            Neurons = new List<Neuron>();
+
+            Neurons.Add(new Neuron(new Vector(100, 100)));
+            Neurons.Add(new Neuron(new Vector(200, 200)));
+
+            foreach(Neuron neuron in Neurons)
+            {
+                neuron.Control.MouseLeftButtonDown += Control_MouseLeftButtonDown;
+                neuron.Control.MouseRightButtonDown += Control_MouseRightButtonDown;
+                AddDrawable(neuron);
+            }
+        }
+
+        // 첫 클릭 => 두번째 클릭으로 branch 생성
+        private void Control_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var neuron = (sender as Shape).Tag as Neuron;
+            if (neuron != null)
+            {
+                if (clicked.Count == 0)
+                {
+                    clicked.Add(neuron);
+                }
+                else if (clicked[0] != neuron)
+                {
+                    clicked[0].AppendOutputBranch(neuron);
+                    neuron.AppendInputBranch(clicked[0]);
+                    clicked.Clear();
+                }
+            }
+            Debug.WriteLine($"어엉어 :{neuron.Value}\n");
+        }
+
+        // 우클릭시 전파
+        private void Control_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var neuron = (sender as Shape).Tag as Neuron;
+            neuron.Propagation();
+            Debug.WriteLine($"하읏 :{neuron.Value}\n");
         }
 
         void AddDrawable(IDrawable item)
         {
             _drawables.Add(item);
             ShapeCanvas.Children.Add(item.Control);
-        }
+        }  
     }
 }
